884de6d25d8c1e7424ce103502367b5849091534
Naycol Linares <wuaicot8@gmail.com>
2024-12-29 21:28:41 -0300

    first commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,165 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+const Page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div
+      style={{
+        position: "absolute",
+        top: "25%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "100vw",
+        height: "200vh",
+        overflow: "hidden",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          width: "400px",
+          height: "400px",
+        }}
+      >
+        <img
+          src="/images/imagen1.png"
+          alt="Descripción de la imagen 1"
+          style={{
+            position: "absolute",
+            top: "-60%",
+            left: "9.3%",
+            transform: "translate(-17%, 92.5%)",
+            zIndex: 2,
+            width: "auto",
+            maxWidth: "100%",
+            height: "500px",
+            maxHeight: "600%",
+          }}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <img
+          src="/images/imagen2.png"
+          alt="Descripción de la imagen 2"
+          style={{
+            position: "absolute",
+            top: "-45%",
+            left: "29.1%",
+            transform: "translate(-50%, 50%)",
+            zIndex: 3,
+            width: "auto",
+            maxWidth: "100%",
+            height: "160%",
+            maxHeight: "200%",
+          }}
+        />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div
+          style={{
+            backgroundColor: "white",
+            position: "absolute",
+            left: "1%",
+            top: "72%",
+            zIndex: 4,
+            width: "177px",
+            height: "5px",
+            visibility: "visible",
+          }}
+        ></div>
+
+        <div
+          style={{
+            backgroundColor: "white",
+            position: "absolute",
+            left: "1%",
+            top: "93%",
+            zIndex: 4,
+            width: "177px",
+            height: "5px",
+            visibility: "visible",
+          }}
+        ></div>
+
+        <div
+          style={{
+            backgroundColor: "white",
+            position: "absolute",
+            left: "1%",
+            top: "114.3%",
+            zIndex: 4,
+            width: "199px",
+            height: "5px",
+            visibility: "visible",
+          }}
+        ></div>
+
+        <div
+          style={{
+            backgroundColor: "white",
+            position: "absolute",
+            left: "1%",
+            top: "136%",
+            zIndex: 4,
+            width: "217px",
+            height: "6px",
+            visibility: "visible",
+          }}
+        ></div>
+
+        <div
+          style={{
+            backgroundColor: "white",
+            position: "absolute",
+            left: "1%",
+            top: "93%",
+            zIndex: 4,
+            width: "177px",
+            height: "5px",
+            visibility: "visible",
+          }}
+        ></div>
+
+        <div
+          style={{
+            backgroundColor: "white",
+            position: "absolute",
+            left: "1%",
+            top: "157%",
+            zIndex: 4,
+            width: "177px",
+            height: "5px",
+            visibility: "visible",
+          }}
+        ></div>
+
+        <img
+          src="/images/imagen3.png"
+          alt="Descripción de la imagen 3"
+          style={{
+            position: "absolute",
+            top: "-15%",
+            left: "5%",
+            transform: "translate(50%, 50%)",
+            zIndex: 1,
+            width: "auto",
+            maxWidth: "100%",
+            height: "130%",
+            maxHeight: "200%",
+          }}
+        />
+        <div
+          style={{
+            color: "black",
+            fontSize: "40px",
+            fontFamily: "  Helvetica",
+          }}
+        >
+          
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      
     </div>
   );
-}
+};
+
+export default Page;
